4e6990d7257c80399881719a975501e4676bc889
Dustin Cobas <dustin.cobas@gmail.com>
2021-01-18 23:46:30 -0300

    Add new benchmarks
    Omit previous executables to avoid compilation problems.

---
+++ bench/bm_build_items.cpp
@@ -0,0 +1,181 @@
+//
+// Created by Dustin Cobas <dustin.cobas@gmail.com> on 1/18/21.
+//
+
+#include <iostream>
+#include <sys/stat.h>
+
+#include <gflags/gflags.h>
+
+#include <benchmark/benchmark.h>
+#include <utils/grammar.h>
+#include <SelfGrammarIndexBS.h>
+#include <SelfGrammarIndexPTS.h>
+
+DEFINE_string(data, "", "Data file. (MANDATORY)");
+DEFINE_bool(rebuild, false, "Rebuild all the items.");
+
+void SetupDefaultCounters(benchmark::State &t_state) {
+  t_state.counters["n"] = 0;
+  t_state.counters["z"] = 0;
+  t_state.counters["s"] = 0;
+  t_state.counters["r'"] = 0;
+  t_state.counters["mr'"] = 0;
+}
+
+// Benchmark Warm-up
+static void BM_WarmUp(benchmark::State &t_state) {
+  for (auto _ : t_state) {
+    std::vector<int> empty_vector(1000000, 0);
+  }
+
+  SetupDefaultCounters(t_state);
+}
+BENCHMARK(BM_WarmUp);
+
+std::string load_data(const std::string &collection);
+
+auto BM_BuildGIndexPT = [](benchmark::State &t_state,
+                           const auto &t_data_path,
+                           const auto &t_basics_fn,
+                           const auto &t_repair_fn,
+                           const auto &t_suffixes_fn) {
+  SelfGrammarIndexBS idx;
+  grammar *not_compressed_grammar = nullptr;
+  std::vector<std::pair<std::pair<size_t, size_t>, std::pair<size_t, size_t> > > grammar_sfx;
+
+  for (auto _ : t_state) {
+    std::string data = load_data(t_data_path);
+
+    if (not_compressed_grammar != nullptr) { delete not_compressed_grammar; }
+    not_compressed_grammar = new grammar();
+    grammar_sfx.clear();
+
+    idx.build_basics(data, *not_compressed_grammar, grammar_sfx);
+  }
+
+  // Store G-Index's basics
+  {
+    std::fstream fbasics(t_basics_fn, std::ios::out | std::ios::binary);
+    idx.save(fbasics);
+  }
+  {
+    std::fstream frepair(t_repair_fn, std::ios::out | std::ios::binary);
+    if (not_compressed_grammar != nullptr) {
+      not_compressed_grammar->save(frepair);
+      delete not_compressed_grammar;
+    }
+  }
+  {
+    std::fstream fsuffixes(t_suffixes_fn, std::ios::out | std::ios::binary);
+    unsigned long num_sfx = grammar_sfx.size();
+    sdsl::serialize(num_sfx, fsuffixes);
+
+    for (auto &&e : grammar_sfx) {
+
+      size_t p = e.first.first;
+      sdsl::serialize(p, fsuffixes);
+      p = e.first.second;
+      sdsl::serialize(p, fsuffixes);
+      p = e.second.first;
+      sdsl::serialize(p, fsuffixes);
+      p = e.second.second;
+      sdsl::serialize(p, fsuffixes);
+
+    }
+  }
+
+  SetupDefaultCounters(t_state);
+};
+
+auto BM_BuildGIndexPTS = [](benchmark::State &t_state,
+                            const auto &t_data_path,
+                            const auto &t_basics_fn,
+                            const auto &t_repair_fn,
+                            const auto &t_suffixes_fn,
+                            const auto &t_pts_idx_fn) {
+  std::size_t s = t_state.range(0);
+
+  SelfGrammarIndexPTS idx(s);
+
+  for (auto _ : t_state) {
+    std::string data = load_data(t_data_path);
+
+    std::fstream fbasics(t_basics_fn, std::ios::in | std::ios::binary);
+    idx.load_basics(fbasics);
+
+    std::fstream fsuffixes(t_suffixes_fn, std::ios::in | std::ios::binary);
+    std::fstream frepair(t_repair_fn, std::ios::in | std::ios::binary);
+
+    idx.build_suffix(data, fsuffixes, frepair);
+  }
+
+  // Store G-IndexPTS
+  std::fstream f_gidx(std::to_string(s) + "_" + t_pts_idx_fn, std::ios::out | std::ios::binary);
+  idx.save(f_gidx);
+
+  SetupDefaultCounters(t_state);
+};
+
+inline bool file_exists(const std::string &name);
+
+int main(int argc, char **argv) {
+  gflags::SetUsageMessage("This program calculates the ri items for the given text.");
+  gflags::AllowCommandLineReparsing();
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  if (FLAGS_data.empty()) {
+    std::cerr << "Command-line error!!!" << std::endl;
+    return 1;
+  }
+
+  std::string data_path = FLAGS_data;
+  std::string basics_fn = "basics.gi";
+  std::string repair_fn = "grepair.gi";
+  std::string suffixes_fn = "suffixes.gi";
+  std::string pts_idx_fn = "pts-idx.gi";
+
+  if (!file_exists(basics_fn) || FLAGS_rebuild) {
+    benchmark::RegisterBenchmark("BuildLZ77Parsing", BM_BuildGIndexPT, data_path, basics_fn, repair_fn, suffixes_fn);
+  }
+
+  if (!file_exists("16_" + pts_idx_fn) || FLAGS_rebuild) {
+    benchmark::RegisterBenchmark("BuildHSIdx",
+                                 BM_BuildGIndexPTS,
+                                 data_path,
+                                 basics_fn,
+                                 repair_fn,
+                                 suffixes_fn,
+                                 pts_idx_fn)
+        ->RangeMultiplier(2)
+        ->Range(2, 2u << 6u);
+  }
+
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
+
+  return 0;
+}
+
+inline bool file_exists(const std::string &name) {
+  struct stat buffer;
+  return (stat(name.c_str(), &buffer) == 0);
+}
+
+std::string load_data(const std::string &collection) {
+  std::fstream f(collection, std::ios::in | std::ios::binary);
+  if (!f.is_open()) {
+    std::cout << "Error the file could not opened!!\n";
+    return "";
+  }
+
+  std::string data;
+  std::string buff;
+  unsigned char buffer[1000];
+  while (!f.eof()) {
+    f.read((char *) buffer, 1000);
+    data.append((char *) buffer, f.gcount());
+  }
+
+  return data;
+}
---
+++ bench/bm_locate.cpp
@@ -0,0 +1,188 @@
+//
+// Created by Dustin Cobas <dustin.cobas@gmail.com> on 1/18/21.
+//
+
+#include <iostream>
+#include <fstream>
+#include <sys/stat.h>
+
+#include <gflags/gflags.h>
+
+#include <benchmark/benchmark.h>
+
+#include <SelfGrammarIndexPTS.h>
+
+DEFINE_string(patterns, "", "Patterns file. (MANDATORY)");
+DEFINE_string(data_dir, "./", "Data directory.");
+DEFINE_string(data_name, "data", "Data file basename.");
+DEFINE_bool(print_result, false, "Execute benchmark that print results per index.");
+
+void SetupDefaultCounters(benchmark::State &t_state) {
+  t_state.counters["Collection_Size(bytes)"] = 0;
+  t_state.counters["Size(bytes)"] = 0;
+  t_state.counters["Bits_x_Symbol"] = 0;
+  t_state.counters["Patterns"] = 0;
+  t_state.counters["Time_x_Pattern"] = 0;
+  t_state.counters["Occurrences"] = 0;
+  t_state.counters["Time_x_Occurrence"] = 0;
+}
+
+// Benchmark Warm-up
+static void BM_WarmUp(benchmark::State &_state) {
+  for (auto _ : _state) {
+    std::vector<int> empty_vector(1000000, 0);
+  }
+
+  SetupDefaultCounters(_state);
+}
+BENCHMARK(BM_WarmUp);
+
+auto BM_QueryLocate = [](benchmark::State &t_state,
+                         const auto &t_idx_dir,
+                         const auto &t_pts_idx_fn,
+                         const auto &t_patterns,
+                         auto t_seq_size) {
+  std::size_t s = t_state.range(0);
+
+  std::fstream fpts(t_idx_dir + "/" + std::to_string(s) + "_" + t_pts_idx_fn, std::ios::in | std::ios::binary);
+  SelfGrammarIndexPTS idx(s);
+  idx.load(fpts);
+
+  std::size_t total_occs = 0;
+
+  for (auto _ : t_state) {
+    total_occs = 0;
+    for (const auto &pattern: t_patterns) {
+      auto pat = pattern;
+      std::vector<uint> occs;
+
+      idx.locate(pat, occs);
+
+      total_occs += occs.size();
+    }
+  }
+
+  SetupDefaultCounters(t_state);
+  t_state.counters["Collection_Size(bytes)"] = t_seq_size;
+  t_state.counters["Size(bytes)"] = idx.size_in_bytes();
+  t_state.counters["Bits_x_Symbol"] = idx.size_in_bytes() * 8.0 / t_seq_size;
+  t_state.counters["Patterns"] = t_patterns.size();
+  t_state.counters["Time_x_Pattern"] = benchmark::Counter(
+      t_patterns.size(), benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
+  t_state.counters["Occurrences"] = total_occs;
+  t_state.counters["Time_x_Occurrence"] = benchmark::Counter(
+      total_occs, benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
+};
+
+auto BM_PrintQueryLocate = [](benchmark::State &t_state,
+                              const auto &t_idx_name,
+                              const auto &t_idx_dir,
+                              const auto &t_pts_idx_fn,
+                              const auto &t_patterns,
+                              auto t_seq_size) {
+  std::size_t s = t_state.range(0);
+
+  std::string idx_name = t_idx_name;
+  replace(idx_name.begin(), idx_name.end(), '/', '_');
+  std::string output_filename = "result-" + idx_name + "-" + std::to_string(s) + ".txt";
+
+  std::fstream fpts(t_idx_dir + "/" + std::to_string(s) + "_" + t_pts_idx_fn, std::ios::in | std::ios::binary);
+  SelfGrammarIndexPTS idx(s);
+  idx.load(fpts);
+
+  std::size_t total_occs = 0;
+
+  for (auto _ : t_state) {
+    std::ofstream out(output_filename);
+    total_occs = 0;
+    for (const auto &pattern: t_patterns) {
+      out << pattern << std::endl;
+      auto pat = pattern;
+      std::vector<uint> occs;
+
+      idx.locate(pat, occs);
+
+      total_occs += occs.size();
+
+      sort(occs.begin(), occs.end());
+      for (const auto &item  : occs) {
+        out << "  " << item << std::endl;
+      }
+    }
+  }
+
+  SetupDefaultCounters(t_state);
+  t_state.counters["Collection_Size(bytes)"] = t_seq_size;
+  t_state.counters["Size(bytes)"] = idx.size_in_bytes();
+  t_state.counters["Bits_x_Symbol"] = idx.size_in_bytes() * 8.0 / t_seq_size;
+  t_state.counters["Patterns"] = t_patterns.size();
+  t_state.counters["Time_x_Pattern"] = benchmark::Counter(
+      t_patterns.size(), benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
+  t_state.counters["Occurrences"] = total_occs;
+  t_state.counters["Time_x_Occurrence"] = benchmark::Counter(
+      total_occs, benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
+};
+long filesize(const std::string &t_filename);
+
+int main(int argc, char **argv) {
+  gflags::AllowCommandLineReparsing();
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  if (FLAGS_patterns.empty() || FLAGS_data_name.empty() || FLAGS_data_dir.empty()) {
+    std::cerr << "Command-line error!!!" << std::endl;
+    return 1;
+  }
+
+  // Query patterns
+  std::vector<std::string> patterns;
+  {
+    std::ifstream pattern_file(FLAGS_patterns.c_str(), std::ios_base::binary);
+    if (!pattern_file) {
+      std::cerr << "ERROR: Failed to open patterns file!" << std::endl;
+      return 3;
+    }
+
+    std::string buf;
+    while (std::getline(pattern_file, buf)) {
+      if (buf.empty())
+        continue;
+
+      patterns.emplace_back(buf);
+    }
+    pattern_file.close();
+  }
+
+  // Indexes
+  std::string pts_idx_fn = "pts-idx.gi";
+  auto coll_size = filesize(FLAGS_data_dir + "/" + FLAGS_data_name);
+
+  std::string index_name = "g-index";
+  benchmark::RegisterBenchmark(index_name.c_str(), BM_QueryLocate, FLAGS_data_dir, pts_idx_fn, patterns, coll_size)
+      ->RangeMultiplier(2)
+      ->Range(2, 1u << 6u);
+
+  std::string print_bm_prefix = "Print-";
+  if (FLAGS_print_result) {
+    auto print_bm_name = print_bm_prefix + index_name;
+    benchmark::RegisterBenchmark(print_bm_name.c_str(),
+                                 BM_PrintQueryLocate,
+                                 index_name,
+                                 FLAGS_data_dir,
+                                 pts_idx_fn,
+                                 patterns,
+                                 coll_size)
+        ->RangeMultiplier(2)
+        ->Range(2, 1u << 6u);
+  }
+
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
+
+  return 0;
+}
+
+long filesize(const std::string &t_filename) {
+  struct stat stat_buf;
+  int rc = stat(t_filename.c_str(), &stat_buf);
+  return rc == 0 ? stat_buf.st_size : -1;
+}
